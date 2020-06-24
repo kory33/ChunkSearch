6b5f0e750d8bb90a625f2b3ae8572d3d35368d4c
Kory <korygm33@gmail.com>
2020-06-25 06:22:45 +0900

    [clean] reformat

--- Program.fs
+++ Program.fs
@@ -7,23 +7,25 @@ open InventorySearch
 let entities folderPath =
     use world = World.FromDirectory(folderPath)
 
-    let result = ResizeArray (seq {
-        for dim in world.Worlds do
-            for tileEntity in dim.GetAllTileEntities() do
-                let coordinate =
-                    { x = Some tileEntity.XCoord
-                      y = Some tileEntity.YCoord
-                      z = Some tileEntity.ZCoord }
+    let result =
+        ResizeArray
+            (seq {
+                for dim in world.Worlds do
+                    for tileEntity in dim.GetAllTileEntities() do
+                        let coordinate =
+                            { x = Some tileEntity.XCoord
+                              y = Some tileEntity.YCoord
+                              z = Some tileEntity.ZCoord }
 
-                let nbt = tileEntity.NBTData
-                if nbt.ContainsKey "Items" || nbt.ContainsKey "RecordItem" then
-                    let record =
-                        { coord = Some coordinate
-                          dimensionId = Some dim.DimensionID
-                          tileEntityId = Some tileEntity.Id }
-                    yield record
-    })
-    
+                        let nbt = tileEntity.NBTData
+                        if nbt.ContainsKey "Items" || nbt.ContainsKey "RecordItem" then
+                            let record =
+                                { coord = Some coordinate
+                                  dimensionId = Some dim.DimensionID
+                                  tileEntityId = Some tileEntity.Id }
+                            yield record
+             })
+
     { result = result }
 
 open Falanx.Proto.Codec.Binary.Primitives
@@ -32,6 +34,6 @@ open Falanx.Proto.Codec.Binary.Primitives
 let main argv =
     let result = entities argv.[0]
     let buffer = ZeroCopyBuffer(int (serializedLength result))
-    SearchResult.Serialize (result, buffer)
+    SearchResult.Serialize(result, buffer)
     printf "%s" (System.Text.Encoding.ASCII.GetString buffer.Array)
     0
